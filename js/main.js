df13ea19cad4e60823d6884cfe11fdb89ecb6778
Igormandello <igormandello@gmail.com>
2018-09-26 09:04:41 -0300

    Starts the page styling

--- js/main.js
+++ js/main.js
@@ -13,5 +13,11 @@ window.onload = () => {
   load(() => {
     let actualClasses = lab();
     console.log(actualClasses);
+
+    let mainP = document.querySelector('p:nth-child(2)');
+    if (actualClasses.some(obj => obj === '-' || obj.includes('Aula Reforço')))
+      mainP.innerText = 'Tem';
+    else
+      mainP.innerText = 'Não tem';
   });
 }
