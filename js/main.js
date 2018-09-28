80d51042bfefd16c45da8ee7020c87433334d173
Igormandello <igormandello@gmail.com>
2018-09-28 10:16:17 -0300

    Updates gh-pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const gistURL = 'https://gist.github.com/Igormandello/a8d8d01fb9997c1120a18a2b1bdf20ad/raw/';
+const gistURL = 'https://gist.githubusercontent.com/Igormandello/a8d8d01fb9997c1120a18a2b1bdf20ad/raw/';
 
 const labNames = [
 	'Dinalva',
